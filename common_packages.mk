40a15e413396f435c2367912273c86b2a4ac5870
Santhoosh <santhosh6194@gmail.com>
2021-06-12 22:11:09 +0530

    m30s-common: build media codec files

--- common_packages.mk
+++ common_packages.mk
@@ -93,6 +93,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.samsung
 
+# MEDIA #
+PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-impl \
+    android.hardware.media.omx@1.0-service
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+
 # MEMTRACK #
 PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl:64 \
